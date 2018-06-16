ccd8ac47ae9f895a393797d975feb8e487cc27ac
NSTF <starlinu33@gmail.com>
2018-06-15 20:20:28 -0400

    Bullenting Borad for user to post messages and see it as well in the other side, saving it into postgreSQL

---
+++ BulletinBoardApp/public/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+      $(`#submitId`).on('click', () => {
+        console.log('You submitted your post');
+      });
+
+      $(".far").on('click', (event) => {
+        let id = $(event.target).parent().attr(`data-Post`);
+        let url = `/delete/` + id;
+        if (confirm(`Are you sure you want to delete ?`)) {
+          $.ajax({
+            url: url,
+            type: 'DELETE',
+            succcess: function() {
+              console.log('Deleting post...');
+              // redirecting
+              windows.location.href = '/';
+            }
+          })
+
+        }
+      })
+    });
